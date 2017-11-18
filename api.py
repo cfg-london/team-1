47683d5c027f6bd556d5e6b756cc55a8ea7971cc
ArmandNM <armand.nicolicioiu@gmail.com>
2017-11-18 11:45:19 +0200

    fixed horizontal & implemented histogram

--- api.py
+++ api.py
@@ -63,7 +63,7 @@ def parse_data(file):
     surveys = extract_surveys(table)
     return surveys
 
-def horizontal_chart(countries, surveys_by_country):
+def horizontal_chart(countries, surveys_by_indicator):
     # dataset = [
     #     {label: "Men", "Not Satisfied": 20, "Not Much Satisfied": 10, "Satisfied": 50, "Very Satisfied": 20},
     #     {label: "Women", "Not Satisfied": 15, "Not Much Satisfied": 30, "Satisfied": 40, "Very Satisfied": 15}
@@ -71,16 +71,52 @@ def horizontal_chart(countries, surveys_by_country):
 
     answer = []
 
-    for c in surveys_by_country.keys():
+    print(surveys_by_indicator)
+
+    for indicator in surveys_by_indicator.keys():
         crt_pairs = {}
-        crt_pairs['label'] = c
-        print(surveys_by_country[c].indicators)
-        for indicator in surveys_by_country[c].indicators:
-            if 'Total' in surveys_by_country[c].indicators[indicator]:
-                crt_pairs[indicator] = surveys_by_country[c].indicators[indicator]['Total']
-                print('adsdasdasdasdasd')
+        crt_pairs['label'] = indicator
+        # print(surveys_by_country[c].indicators)
+        for c in surveys_by_indicator[indicator]:
+            # print(c)
+            if 'Total' in surveys_by_indicator[indicator][c]:
+                print('yes')
+                # print(surveys_by_indicator[indicator][c].indicators[indicator])
+                crt_pairs[c] = surveys_by_indicator[indicator][c]['Total']
+                # print('adsdasdasdasdasd')
+                # print(crt_pairs)
         ordered_dict = collections.OrderedDict(sorted(crt_pairs.items()))
         answer.append(ordered_dict)
+
+    return json.dumps(answer)
+
+def hisogram(countries, indicator, surveys_by_country):
+    answer = []
+
+    pairs_by_country = {}
+    for country in surveys_by_country.keys():
+        pairs_by_country[country] = surveys_by_country[country].indicators[indicator]
+
+    print('asdasdasdas')
+    print(pairs_by_country)
+
+    for category in pairs_by_country[list(pairs_by_country.keys())[0]]:
+        crt_bars = {}
+        crt_bars["categorie"] = category
+
+        values = []
+        for country in pairs_by_country.keys():
+            print(pairs_by_country[country][category])
+            values.append({'value': pairs_by_country[country][category], 'rate': country})
+
+        crt_bars["values"] = values
+
+        answer.append(crt_bars)
+
+        # print(c)
+        # print(surveys_by_country[c].indicators[indicator])
+        # for category in surveys_by_country[c].indicators[indicator]:
+
 
     return json.dumps(answer)
 
@@ -100,4 +136,19 @@ if __name__ == "__main__":
         if s.indicators['Indicator']['Country'].startswith('Senegal') and s.indicators['Indicator']['Survey'].startswith('2016 DHS'):
             surveys_by_country['Senegal'] = s
 
-    print(horizontal_chart(['Indonesia', 'India'], surveys_by_country))
+    print('sbc')
+    print(surveys_by_country)
+
+    surveys_by_indicator = {}
+    for c in surveys_by_country.keys():
+        for i in surveys_by_country[c].indicators:
+            if i not in surveys_by_indicator:
+                surveys_by_indicator[i] = {}
+            surveys_by_indicator[i][c] = surveys_by_country[c].indicators[i]
+
+    print(len(surveys_by_indicator.keys()))
+
+    # print(horizontal_chart(['Indonesia', 'India'], surveys_by_indicator))
+    # print(hisogram(['Indonesia', 'India'], 'Wife beating justified for at least one specific reason [Women]', surveys_by_country))
+
+    print(horizontal_chart(['Indonesia', 'India'], surveys_by_indicator))
